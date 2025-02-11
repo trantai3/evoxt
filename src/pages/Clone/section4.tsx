ec3b311b3fb2d6efaf59e0fb9719fbfa48e4ec77
Tai <trantai09032003@gmail.com>
2025-02-11 15:45:47 +0700

    Section5 ClonePage

---
+++ src/pages/Clone/section4.tsx
@@ -0,0 +1,15 @@
+import "../../css/Clone/Section4/style.scss";
+const Section4 = () => {
+  return (
+    <div className="clonePage">
+      <div className="section__4">
+        <div className="container">
+          <h2>The best part? It is completely free!</h2>
+          <p>Evoxt don't charge for any cloning services</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section4;
---
+++ src/pages/Clone/section5.tsx
@@ -0,0 +1,18 @@
+import "../../css/Clone/Section5/style.scss";
+const Section5 = () => {
+  return (
+    <div className="clonePage">
+      <div className="section__5">
+        <div className="container">
+          <h3>FAQ</h3>
+          <ul>
+            <li>How long will this take?</li>– This usually takes around 5
+            minutes, bigger drives might take slightly longer.
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section5;
