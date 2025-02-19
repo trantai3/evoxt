881b95cc05c91a656f515fd73c1be9ab38f3db51
Tai <trantai09032003@gmail.com>
2025-02-19 11:21:05 +0700

    Update Header LayoutMain

--- src/layouts/LayoutMain/Header/index.tsx
+++ src/layouts/LayoutMain/Header/index.tsx
@@ -24,37 +24,26 @@ const Header = () => {
   }, []);
   return (
     <>
-      <div className="header flex flex-col">
-        <div className="header__one flex justify-end items-center p-[10px] text-white">
-          <a
-            className="header__login underline-none text-[14px] px-[10px] py-0"
-            href="https://console.evoxt.com/login"
-          >
+      <div className="header">
+        <div className="header__one">
+          <a className="header__login" href="https://console.evoxt.com/login">
             Login
           </a>
-          <div className="header__register rounded-[50px]">
-            <Link
-              to="/deploy/dashboard"
-              className="underline-none text-[12px] px-[10px] py-0"
-            >
-              Register
-            </Link>
+          <div className="header__register">
+            <Link to="/deploy/dashboard">Register</Link>
           </div>
           <a
-            className="header__environment ml-[20px] underline-none text-[14px] px-[10px] py-0"
+            className="header__environment"
             href="https://climate.stripe.com/tzMi7z"
           >
             We care about the Environment
           </a>
-          <img
-            src={icon}
-            className="w-[30px] mr-[10px] h-auto max-w-full"
-          ></img>
+          <img src={icon} className="icon-environment" />
         </div>
-        <div className="header__two text-white text-center py-[20px] px-[40px]">
-          <div className="header__container flex flex-wrap justify-between items-center">
-            <a href="/#">
-              <img src={logo} className="max-w-[230px] h-auto" alt="" />
+        <div className="header__two">
+          <div className="header__container">
+            <a href="/">
+              <img src={logo} className="logo" alt="" />
             </a>
             <div
               onClick={() => setIsBars(!isBars)}
@@ -63,45 +52,35 @@ const Header = () => {
               <FaBars />
             </div>
             {screenWidth > 767 && (
-              <div className="header__right md:gap-[40px] 2xl:gap-[60px] flex justify-between">
-                <Link className="text-[15px] deploy font-medium" to="#">
+              <div className="header__right header__right-minwidth767 md:gap-[40px] 2xl:gap-[60px] flex justify-between">
+                <Link className="deploy" to="#">
                   <div
                     onClick={() => setIsClickSubMenu(!isClickSubMenu)}
-                    className="deploy-menu py-[20px] px-[10px] relative flex items-center	"
+                    className="deploy-menu"
                   >
                     Deploy
-                    <FaAngleDown className="ml-[15px]" />
+                    <FaAngleDown className="icon-down" />
                   </div>
-                  <div className="submenu hidden absolute text-left py-[10px]">
-                    <ul className="bg-[#0F1E53]">
-                      <li className="py-[8px] px-[10px]">
-                        <Link className="text-[14px]" to="/deploy">
-                          Cloud Virtual Machines
-                        </Link>
+                  <div className="submenu">
+                    <ul>
+                      <li>
+                        <Link to="/deploy">Cloud Virtual Machines</Link>
                       </li>
-                      <li className="py-[8px] px-[10px]">
-                        <Link
-                          className="text-[14px]"
-                          to="/malaysia-dedicated-server"
-                        >
+                      <li>
+                        <Link to="/malaysia-dedicated-server">
                           Dedicated Servers
                         </Link>
                       </li>
                     </ul>
                   </div>
                   {screenWidth < 768 && isClickSubMenu && (
-                    <div className="submenubars text-left py-[10px] border-b border-[#000]">
-                      <ul className="bg-[#0F1E53]">
-                        <li className="py-[8px] px-[10px]">
-                          <Link className="text-[14px]" to="/deploy">
-                            Cloud Virtual Machines
-                          </Link>
+                    <div className="submenubars submenubars-maxwidth768">
+                      <ul>
+                        <li>
+                          <Link to="/deploy">Cloud Virtual Machines</Link>
                         </li>
-                        <li className="py-[8px] px-[10px]">
-                          <Link
-                            className="text-[14px]"
-                            to="/malaysia-dedicated-server"
-                          >
+                        <li>
+                          <Link to="/malaysia-dedicated-server">
                             Dedicated Servers
                           </Link>
                         </li>
@@ -109,66 +88,47 @@ const Header = () => {
                     </div>
                   )}
                 </Link>
-                <Link
-                  className="elementMenu py-[20px] text-[15px] font-medium"
-                  to="/status"
-                >
+                <Link className="elementMenu" to="/status">
                   Status
                 </Link>
-                <Link
-                  className="elementMenu py-[20px] text-[15px] font-medium"
-                  to="/pricing"
-                >
+                <Link className="elementMenu" to="/pricing">
                   Pricing
                 </Link>
-                <Link
-                  className="elementMenu py-[20px] text-[15px] font-medium"
-                  to="/support/submitticket"
-                >
+                <Link className="elementMenu" to="/support/submitticket">
                   Contact Us
                 </Link>
               </div>
             )}
             {isBars && (
-              <div className="header__right md:gap-[40px] 2xl:gap-[60px] !flex justify-between">
-                <a className="text-[15px] deploy font-medium" href="#">
+              <div className="header__right header__right-bars">
+                <a className="deploy" href="#">
                   <div
                     onClick={() => setIsClickSubMenu(!isClickSubMenu)}
-                    className="deploy-menu py-[20px] px-[10px] relative flex items-center	"
+                    className="deploy-menu"
                   >
                     Deploy
-                    <FaAngleDown className="ml-[15px]" />
+                    <FaAngleDown className="" />
                   </div>
-                  <div className="submenu hidden absolute text-left py-[10px]">
-                    <ul className="bg-[#0F1E53]">
-                      <li className="py-[8px] px-[10px]">
-                        <Link className="text-[14px]" to="/deploy">
-                          Cloud Virtual Machines
-                        </Link>
+                  <div className="submenu">
+                    <ul>
+                      <li>
+                        <Link to="/deploy">Cloud Virtual Machines</Link>
                       </li>
-                      <li className="py-[8px] px-[10px]">
-                        <Link
-                          className="text-[14px]"
-                          to="/malaysia-dedicated-server"
-                        >
+                      <li>
+                        <Link to="/malaysia-dedicated-server">
                           Dedicated Servers
                         </Link>
                       </li>
                     </ul>
                   </div>
                   {isClickSubMenu && (
-                    <div className="submenubars text-left py-[10px] border-b border-[#000]">
-                      <ul className="bg-[#0F1E53]">
-                        <li className="py-[8px] px-[10px]">
-                          <Link className="text-[14px]" to="/deploy">
-                            Cloud Virtual Machines
-                          </Link>
+                    <div className="submenubars submenubars-clickSub">
+                      <ul>
+                        <li>
+                          <Link to="/deploy">Cloud Virtual Machines</Link>
                         </li>
-                        <li className="py-[8px] px-[10px]">
-                          <Link
-                            className="text-[14px]"
-                            to="/malaysia-dedicated-server"
-                          >
+                        <li>
+                          <Link to="/malaysia-dedicated-server">
                             Dedicated Servers
                           </Link>
                         </li>
@@ -176,22 +136,13 @@ const Header = () => {
                     </div>
                   )}
                 </a>
-                <Link
-                  className="elementMenu px-[10px] py-[20px] text-[15px] font-medium"
-                  to="/status"
-                >
+                <Link className="elementMenu" to="/status">
                   Status
                 </Link>
-                <Link
-                  className="elementMenu px-[10px] py-[20px] text-[15px] font-medium"
-                  to="/pricing"
-                >
+                <Link className="elementMenu" to="/pricing">
                   Pricing
                 </Link>
-                <Link
-                  className="elementMenu px-[10px] py-[20px] text-[15px] font-medium"
-                  to="/support/submitticket"
-                >
+                <Link className="elementMenu" to="/support/submitticket">
                   Contact Us
                 </Link>
               </div>
