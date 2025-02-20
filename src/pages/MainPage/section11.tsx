6c7cf6936ad15e0bccf099f5c9207e2a38ab5a37
Tai <trantai09032003@gmail.com>
2025-02-20 15:06:48 +0700

    Update Section11 MainPage

--- src/pages/MainPage/section11.tsx
+++ src/pages/MainPage/section11.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../../css/MainPage/Section11/style.scss";
 const Section11 = () => {
   return (
@@ -9,8 +10,12 @@ const Section11 = () => {
             Create an account with Evoxt now or Contact us to learn more.
           </div>
           <div className="section__11-buttons-wrapper">
-            <div className="button button-account">Create Account</div>
-            <div className="button button-contact">Contact Us</div>
+            <div className="button button-account">
+              <Link to="/deploy/dashboard">Create Account</Link>
+            </div>
+            <div className="button button-contact">
+              <Link to="/deploy/submitticket">Contact Us</Link>
+            </div>
           </div>
         </div>
       </div>
